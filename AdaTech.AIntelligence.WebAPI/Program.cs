bd5ebbaab9b962277571d7708c1ef7976b1f9c78
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-07 10:08:42 -0300

    add db context

--- AdaTech.AIntelligence.WebAPI/Program.cs
+++ AdaTech.AIntelligence.WebAPI/Program.cs
@@ -1,6 +1,17 @@
+using AdaTech.AIntelligence.DateLibrary.Context;
+using AdaTech.AIntelligence.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ExpenseReportingDbContext>();
+
+// Configuração do Identity
+//builder.Services.AddIdentity<UserInfo, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ExpenseReportingDbContext>()
+//    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
