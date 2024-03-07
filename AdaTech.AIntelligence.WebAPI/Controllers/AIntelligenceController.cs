75a4775bb5b9d3251ef7c29c47e945bf08880666
Montaguine <yuria.cifuentes@gmail.com>
2024-03-07 02:17:14 -0300

    feat: adicionou rotas para teste do JWT

---
+++ AdaTech.AIntelligence.WebAPI/Controllers/AIntelligenceController.cs
@@ -0,0 +1,37 @@
+﻿using AdaTech.AIntelligence.Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdaTech.AIntelligence.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AIntelligenceController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        public AIntelligenceController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("criarToken")]
+        public IActionResult CriarToken(string user, string password)
+        {
+            var token = _tokenService.GenerateToken(user, password);
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = System.DateTime.Now.AddHours(1)
+            };
+            Response.Cookies.Append("jwt", token, cookieOptions);
+            return Ok(token);
+        }
+
+        [HttpPost("deletarToken")]
+        public IActionResult DeletarToken()
+        {
+            Response.Cookies.Delete("jwt");
+            return Ok("Token deletado");
+        }
+    }
+}
