277c0e8ab132e0ac89ccadc7390bce4f18b91014
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-07 18:57:09 -0300

    add: create user service interface

---
+++ AdaTech.AIntelligence.Service/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace AdaTech.AIntelligence.Service.Services
+{
+    public interface IUserService
+    {
+        Task<(bool Succeeded, string Role)> AuthenticateAsync(string email, string password);
+        Task<bool> RegisterUserAsync(string email, string password);
+        Task LogoutAsync();
+    }
+}
