422c64de6ce040d607cd0b714604e1158c312fdf
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-08 15:27:21 -0300

    add tests

--- AdaTech.AIntelligence.Tests/UserAuthServiceTests.cs
+++ AdaTech.AIntelligence.Tests/UserAuthServiceTests.cs
@@ -3,7 +3,36 @@ namespace AdaTech.AIntelligence.Tests
     public class UserAuthServiceTests
     {
         [Fact]
-        public void Test1()
+        public void AuthenticateAsync_ShouldSignIn_WhenUserDataIsValid()
+        {
+
+        }
+
+        [Fact]
+        public void AuthenticateAsync_ShouldThrow_WhenUserDataIsNotValid()
+        {
+
+        }
+
+        [Fact]
+        public void LogoutAsync_ShouldLogout_WhenUserIsLoggedIn()
+        {
+
+        }
+        [Fact]
+        public void LogoutAsync_ShouldThrow_WhenUserIsNotLoggedIn()
+        {
+
+        }
+
+        [Fact]
+        public void RegisterUserAsync_ShouldThrow_WhenUserDataIsNotValid()
+        {
+
+        }
+
+        [Fact]
+        public void RegisterUserAsync_ShouldRegisterUser_WhenUserDataIsValid()
         {
 
         }
