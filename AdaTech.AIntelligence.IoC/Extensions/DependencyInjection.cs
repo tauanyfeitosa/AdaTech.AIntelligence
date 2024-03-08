fffbaafcba56a3bb77c5e6db0f64cef5148e6f2e
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-07 23:12:22 -0300

    feat: adiciona injeção de dependências para serviços e configurações
    Implementa a injeção de dependências para serviços essenciais, incluindo TokenService, MustHaveAToken, e SeedUserInitial, além de registrar a configuração personalizada e o serviço hospedado StartupHostedApplication. Esse ajuste aprimora a modularidade e facilita a gestão das dependências em toda a solução.

---
+++ AdaTech.AIntelligence.IoC/Extensions/DependencyInjection.cs
@@ -0,0 +1,42 @@
+﻿using AdaTech.AIntelligence.Ioc.Filters;
+using AdaTech.AIntelligence.IoC.Extensions.ApplicationInitializer;
+using AdaTech.AIntelligence.Service.Services;
+using AdaTech.AIntelligence.Service.Services.SeedUser;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AdaTech.AIntelligence.WebAPI")]
+namespace AdaTech.AIntelligence.IoC.Extensions
+{
+    /// <summary>
+    /// Destinado a injetar as dependências do ciclo de vida da solution.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    internal static class DependencyInjection
+    {
+
+        internal static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.ResolveDependenciesService()
+                    //.ResolveDependenciesRepository()
+                    .AddCustomConfiguration(configuration);
+                    //.AddCustomSwagger()
+                    //.AddSerilog(LoggingConfiguration.ConfigureSerilog(configuration));
+
+            services.AddHostedService<StartupHostedApplication>();
+
+            return services;
+        }
+
+        private static IServiceCollection ResolveDependenciesService(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<MustHaveAToken>();
+            services.AddScoped<ISeedUserInitial, SeedUserInitial>();
+
+            return services;
+        }
+    }
+}
