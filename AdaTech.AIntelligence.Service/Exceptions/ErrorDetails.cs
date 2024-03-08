fcaafa08f420f59f0088bc7292716eb9b5475131
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2024-03-08 11:43:39 -0300

    feat: adicionando classe ErrorDetails

---
+++ AdaTech.AIntelligence.Service/Exceptions/ErrorDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("AdaTech.AIntelligence.WebAPI")]
+namespace AdaTech.AIntelligence.Service.Exceptions
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
