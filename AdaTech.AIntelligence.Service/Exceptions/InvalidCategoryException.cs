14d5e10fa538084bf6bbc1411a98fa0a3db1c767
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2024-03-08 11:58:32 -0300

    feat: adicionando erros customizados

---
+++ AdaTech.AIntelligence.Service/Exceptions/InvalidCategoryException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Service.Exceptions
+{
+    public class InvalidCategoryException : Exception
+    {
+        public InvalidCategoryException() : base() { }
+        public InvalidCategoryException(string message) : base(message) { }
+        public InvalidCategoryException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ AdaTech.AIntelligence.Service/Exceptions/ReadingCategoryException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Service.Exceptions
+{
+    public class ReadingCategoryException : Exception
+    {
+        public ReadingCategoryException() : base() { }
+        public ReadingCategoryException(string message) : base(message) { }
+        public ReadingCategoryException(string message, Exception inner) : base(message, inner) { }
+    }
+}
