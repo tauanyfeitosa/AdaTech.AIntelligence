b4c22c329b5a7a4ac5c589f524d178d70b6cecc9
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-07 23:11:03 -0300

    feat: configuração dos app settings para UserCredentials e TokenUserSettings
    Adiciona configuração personalizada no IServiceCollection para permitir o uso das seções UserCredentials e TokenUserSettings do appsettings.json por meio de IOptions. Isso facilita a injeção dessas configurações em serviços e controladores, melhorando a modularidade e a manutenção do código.

---
+++ AdaTech.AIntelligence.IoC/Extensions/ConfigureAppSettings.cs
@@ -0,0 +1,24 @@
+﻿using AdaTech.AIntelligence.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdaTech.AIntelligence.IoC.Extensions
+{
+    
+    internal static class ConfigureAppSettings
+    {
+        /// <summary>
+        /// Registra as classes de configuração do appsettings para uso injetado com IOptions
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration">provedor de configurações</param>
+        /// <returns></returns>
+        public static IServiceCollection AddCustomConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<UserCredentialsSettings>(configuration.GetSection("UserCredentials"));
+            services.Configure<TokenSettings>(configuration.GetSection("TokenUserSettings"));
+
+            return services;
+        }
+    }
+}
