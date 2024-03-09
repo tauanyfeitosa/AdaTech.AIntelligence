a7d770127370e337f51a50d2c8dee5603e957eb8
Montaguine <yuria.cifuentes@gmail.com>
2024-03-09 20:35:22 -0300

    feat: Injeta dependencia de UserAuthService

--- AdaTech.AIntelligence.Service/Services/IUserAuthService.cs
+++ AdaTech.AIntelligence.Service/Services/IUserAuthService.cs
@@ -1,9 +1,12 @@
-﻿namespace AdaTech.AIntelligence.Service.Services
+﻿using AdaTech.AIntelligence.Entities.Objects;
+using AdaTech.AIntelligence.Service.DTOs;
+
+namespace AdaTech.AIntelligence.Service.Services
 {
     public interface IUserAuthService
     {
         Task<bool> AuthenticateAsync(string email, string password);
-        Task<bool> RegisterUserAsync(string email, string password);
+        Task<bool> RegisterUserAsync(DTOUserRegister userRegister);
         Task LogoutAsync();
     }
 }
