e724f0c698fbd4ae0fb71ad23298d4a3b0536ca3
Montaguine <yuria.cifuentes@gmail.com>
2024-03-09 20:35:53 -0300

    add: Cria DTO para UserRegister

---
+++ AdaTech.AIntelligence.Service/DTOs/DTOUserRegister.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Service.DTOs
+{
+    public class DTOUserRegister
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateBirth { get; set; }
+        public string Password { get; set; }
+        public string CPF { get; set; }
+    }
+}
