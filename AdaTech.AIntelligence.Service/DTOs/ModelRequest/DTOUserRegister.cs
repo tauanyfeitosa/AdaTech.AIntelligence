8eb2dc81afbb4372f2da334a8214af23add85750
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-10 15:34:22 -0300

    feat (Attribute): criação de atributo customizado para emails
    Afim de ter certeza que apenas funcionários da empresa poderão se cadastrar no sistema, o programa reconhece apenas emails do domínio informado no appsettings (neste caso, da empresa Mercado Eletrônico)

--- AdaTech.AIntelligence.Service/DTOs/ModelRequest/DTOUserRegister.cs
+++ AdaTech.AIntelligence.Service/DTOs/ModelRequest/DTOUserRegister.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using AdaTech.AIntelligence.Service.Attributes;
 
-namespace AdaTech.AIntelligence.Service.DTOs
+namespace AdaTech.AIntelligence.Service.DTOs.ModelRequest
 {
     public class DTOUserRegister
     {
+        [Required(ErrorMessage = "O campo Email é obrigatório!")]
+        [EmailAddress(ErrorMessage = "O campo Email é inválido!")]
+        [EmailDomainAttribute("EmailSettings:Domain")]
         public string Email { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
