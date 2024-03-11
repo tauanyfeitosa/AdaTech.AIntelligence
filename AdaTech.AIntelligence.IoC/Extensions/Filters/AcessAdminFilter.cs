d1a234d5fd818a642935b4474d7dee84ea585867
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-11 13:36:23 -0300

    feat (Filter): criação de filtros para controlar acessos segundo as regras de autorização do identity

---
+++ AdaTech.AIntelligence.IoC/Extensions/Filters/AcessAdminFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AdaTech.AIntelligence.IoC.Extensions.Filters
+{
+    public class AcessAdminFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            
+            var isSuperUser = user.IsInRole("Admin");
+
+            if (!isSuperUser)
+            {
+                throw new UnauthorizedAccessException("Acesso negado");
+            }
+        }
+    }
+}
