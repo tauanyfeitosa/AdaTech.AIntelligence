85de4a00ed839baa5d24c9358e054ef3037aa752
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-11 10:04:11 -0300

    feat (connection): criação de método para verificar a conexão com o chatGPT

---
+++ AdaTech.AIntelligence.Service/Services/ChatGPTService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+
+namespace AdaTech.AIntelligence.Service.Services
+{
+    public static class ChatGPTService
+    {
+        public static async Task<string> GenerateResponse(this string apiKey, IHttpClientFactory clientFactory)
+        {
+            using var httpClient = clientFactory.CreateClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+            HttpResponseMessage response;
+            try
+            {
+                response = await httpClient.GetAsync("https://api.openai.com/v1/engines");
+            }
+            catch (HttpRequestException)
+            {
+                throw new Exception("Erro ao se conectar ao chat GPT.");
+            }
+
+            if (response.IsSuccessStatusCode)
+            {
+                return "Conexão com o chat GPT bem-sucedida!";
+            }
+            
+            throw new Exception("Erro ao conectar-se ao chat GPT.");
+        }
+    }
+}
---
+++ AdaTech.AIntelligence.WebAPI/Controllers/ChatGPTController.cs
@@ -0,0 +1,32 @@
+﻿using AdaTech.AIntelligence.Service.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+
+namespace AdaTech.AIntelligence.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatGPTController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ChatGPTController(IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _clientFactory = clientFactory;
+        }
+
+
+        [HttpGet("check")]
+        public async Task<IActionResult> CheckGptConnection()
+        {
+            var apiKey = _configuration.GetValue<string>("ApiKey");
+
+            var response = await apiKey.GenerateResponse(_clientFactory);
+
+            return Ok(response);
+        }
+    }
+}
