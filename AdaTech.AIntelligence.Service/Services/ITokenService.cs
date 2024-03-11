337ed7d4aecac48a250ae88319f54c724fe68656
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-11 13:34:00 -0300

    refactor: retirada a obrigatoriedade do uso do JWT
    o Identity está criando Claims principais para os usuários logados, enquanto não for acordado qual a melhor solução para isso, optei pelo desvinculamento do jwt

---
+++ AdaTech.AIntelligence.Service/Services/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using AdaTech.AIntelligence.Entities.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Service.Services
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(UserInfo user);
+    }
+}
