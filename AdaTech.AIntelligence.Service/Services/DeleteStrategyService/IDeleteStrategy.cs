bedf2fa00874040beaf778654c793cdc12c05440
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-11 21:02:57 -0300

    add delete strategy interface and migration

---
+++ AdaTech.AIntelligence.Service/Services/DeleteStrategyService/IDeleteStrategy.cs
@@ -0,0 +1,11 @@
+﻿using AdaTech.AIntelligence.DateLibrary.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AdaTech.AIntelligence.Service.Services.DeleteStrategyService
+{
+    public interface IDeleteStrategy<T> where T : class
+    {
+        Task<string> DeleteAsync(IAIntelligenceRepository<T> repository, ILogger logger, DbContext context, int id);
+    }
+}
