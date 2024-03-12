0097d602126afd02a29e2108377d32b250346d70
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2024-03-12 11:48:30 -0300

    feat: adiciona classe image

---
+++ AdaTech.AIntelligence.Entities/Objects/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Entities.Objects
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public byte[]? ByteImage { get; set; }
+        public string Path { get; set; }
+    }
+}
