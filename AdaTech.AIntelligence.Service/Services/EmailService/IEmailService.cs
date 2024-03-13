b4fce3eb962d6805174f78e3a369c6398eb4b3d3
Montaguine <yuria.cifuentes@gmail.com>
2024-03-13 17:23:56 -0300

    feat: cria EmailService para tratar envio de email

---
+++ AdaTech.AIntelligence.Service/Services/EmailService/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿using AdaTech.AIntelligence.Entities.Objects;
+
+namespace AdaTech.AIntelligence.Service.Services.EmailService
+{
+    public interface IEmailService
+    {
+        public Task SendEmailAsync(string email, string subject, string message);
+        public Task ConfirmEmailAsync(UserInfo user, string token);
+
+    }
+}
