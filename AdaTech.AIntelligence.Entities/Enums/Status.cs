f32b9667b1f67d3f3e97e551444cc0c45fd3e007
Montaguine <yuria.cifuentes@gmail.com>
2024-03-15 11:04:59 -0300

    add: Enum de controle de PromoteUser adicionado

---
+++ AdaTech.AIntelligence.Entities/Enums/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AIntelligence.Entities.Enums
+{
+    public enum Status
+    {
+        Rejected = 0,
+        None = 1,
+        Requested = 2,
+        Approved = 3,
+    }
+}
