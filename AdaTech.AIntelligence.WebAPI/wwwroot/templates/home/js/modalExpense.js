4cef13ab2c2538d13c562229d79c576244f4ef3f
Charles Serafim Morais <charles.serafim.morais@gmail.com>
2024-03-24 12:16:54 -0300

    feat: modificações na usabilidade de criar despesa

--- AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/modalExpense.js
+++ AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/modalExpense.js
@@ -41,7 +41,18 @@ function setupOptions() {
 
     optionsSelect.addEventListener('change', function () {
         toggleInputFields(this.value);
+        if (this.value === 'url') {
+            document.getElementById('file-input').value = '';
+            document.getElementById('file-label-2').textContent = 'Nenhum arquivo selecionado';
+            document.getElementById('botao-criar-despesa').disabled = true;
+            document.getElementById('url-input').focus();
+        }
+        if (this.value === 'file') {
+            document.getElementById('url-input').value = '';
+            document.getElementById('botao-criar-despesa').disabled = true;
+        }
     });
+
 
     toggleInputFields(optionsSelect.value);
 }
@@ -50,11 +61,47 @@ function toggleInputFields(selectedOption) {
     const fileInput = document.getElementById('file-input');
     const urlInput = document.getElementById('url-input');
     const fileLabel = document.getElementById('file-label');
+    const fileLabel2 = document.getElementById('file-label-2');
     const urlLabel = document.getElementById('url-label');
 
-    fileInput.style.display = selectedOption === 'file' ? 'block' : 'none';
-    fileLabel.style.display = selectedOption === 'file' ? 'block' : 'none';
-    urlInput.style.display = selectedOption === 'url' ? 'block' : 'none';
-    urlLabel.style.display = selectedOption === 'url' ? 'block' : 'none';
+    if (selectedOption === 'url') {
+        fileLabel.style.display = 'none';
+        fileLabel2.style.display = 'none';
+        urlInput.style.display = 'block';
+        urlLabel.style.display = 'block';
+    }
+    else {
+        fileLabel.style.display = 'block';
+        fileLabel2.style.display = 'block';
+        urlInput.style.display = 'none';
+        urlLabel.style.display = 'none';
+    }
 }
 
+
+const btnCriarDespesa = document.getElementById('botao-criar-despesa');
+
+document.getElementById('file-input').addEventListener('change', function () {
+    const fileLabel2 = document.querySelector('#file-label-2');
+    if (this.value) {
+        fileLabel2.textContent = this.files[0].name;
+    } else {
+        fileLabel2.textContent = 'Nenhum arquivo selecionado';
+    }
+});
+
+document.getElementById('file-input').addEventListener('change', function () {
+    if (this.files.length > 0 || this.value) {
+        btnCriarDespesa.disabled = false;
+    } else {
+        btnCriarDespesa.disabled = true;
+    }
+});
+
+document.getElementById('url-input').addEventListener('input', function () {
+    if (this.value.trim() !== '') {
+        btnCriarDespesa.disabled = false;
+    } else {
+        btnCriarDespesa.disabled = true;
+    }
+});
