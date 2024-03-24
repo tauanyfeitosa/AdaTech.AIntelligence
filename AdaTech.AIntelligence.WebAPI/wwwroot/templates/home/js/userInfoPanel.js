9d5f382a120e895cf26e065ed6ffb61089824cad
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-24 16:36:39 -0300

    style: modificação no painel de informações do usuário

--- AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/userInfoPanel.js
+++ AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/userInfoPanel.js
@@ -1,36 +1,36 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://localhost:7016/api/UserManagement/view-user-logged')
-        .then(response => response.json())
-        .then(userData => {
-            const user = userData.values;
-            const name = `${user.name} ${user.lastName}`;
-            const cpf = formatarCPF(user.cpf);
-            const userNameElement = document.createElement('p');
-            const userCpfElement = document.createElement('p');
-            userNameElement.textContent = `Nome: ${name}`;
-            userCpfElement.textContent = `CPF: ${cpf}`;
-            const userDetailsElement = document.querySelector('.user-details');
-            userDetailsElement.appendChild(userNameElement);
-            userDetailsElement.appendChild(userCpfElement);
-        })
-        .catch(error => console.error('Erro ao carregar dados do usuário:', error));
+﻿document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        const userResponse = await fetch('https://localhost:7016/api/UserManagement/view-user-logged');
+        const userData = await userResponse.json();
+        const user = userData.values;
+        const name = `${user.name} ${user.lastName}`;
+        const cpf = formatarCPF(user.cpf);
 
-    fetch('https://localhost:7016/api/UserManagement/view-role-user-logged')
-        .then(response => response.json())
-        .then(rolesData => {
-            if (Array.isArray(rolesData.values)) {
-                const rolesList = rolesData.values.join(', ');
-                const userRolesElement = document.createElement('p');
-                userRolesElement.textContent = `Cargo(s): ${rolesList}`;
-                const userDetailsElement = document.querySelector('.user-details');
-                if (userDetailsElement) {
-                    userDetailsElement.appendChild(userRolesElement);
-                }
-            } else {
-                console.error('rolesData não é uma array:', rolesData);
-            }
-        })
-        .catch(error => console.error('Erro ao carregar cargos do usuário:', error));
+        const userDetailsElement = document.querySelector('.user-details');
+
+        const userNameElement = document.createElement('p');
+        userNameElement.innerHTML = `<span class="label">Nome:</span> ${name}`;
+        userDetailsElement.appendChild(userNameElement);
+
+        const userCpfElement = document.createElement('p');
+        userCpfElement.innerHTML = `<span class="label">CPF:</span> ${cpf}`;
+        userDetailsElement.appendChild(userCpfElement);
+
+        const rolesResponse = await fetch('https://localhost:7016/api/UserManagement/view-role-user-logged');
+        const rolesData = await rolesResponse.json();
+
+        if (Array.isArray(rolesData.values)) {
+            const rolesList = rolesData.values.join(', ');
+            const userRolesElement = document.createElement('p');
+            userRolesElement.innerHTML = `<span class="label">Cargo(s):</span> ${rolesList}`;
+            userDetailsElement.appendChild(userRolesElement);
+            userDetailsElement.appendChild(userRolesElement);
+        } else {
+            console.error('rolesData não é uma array:', rolesData);
+        }
+    } catch (error) {
+        console.error('Erro ao carregar dados do usuário:', error);
+    }
 });
 
 function formatarCPF(cpf) {
