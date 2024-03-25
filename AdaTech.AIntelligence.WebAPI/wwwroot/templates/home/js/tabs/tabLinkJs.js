dd7f028d2c0b042446597cbc0ae0b96650d71d38
Tauany Feitosa <tauanysanttos13@gmail.com>
2024-03-25 00:20:32 -0300

    fix: impossibilitar  o acesso a abas que não condizem com a permissão

--- AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/tabs/tabLinkJs.js
+++ AdaTech.AIntelligence.WebAPI/wwwroot/templates/home/js/tabs/tabLinkJs.js
@@ -1,5 +1,30 @@
-﻿function openTab(evt, tabName) {
+﻿let globalRoles = [];
+
+async function getUserRolesTab() {
+    const response = await fetch('https://localhost:7016/api/UserManagement/view-role-user-logged');
+    if (response.ok) {
+        const roles = await response.json();
+        globalRoles = roles.values;
+    }
+}
+
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
+
+    console.log('Roles in tab:', globalRoles);
+
+    console.log('Tab name:', tabName);
+
+    if (tabName === 'Admin' && !globalRoles.includes('Admin')) {
+        alert('Você não tem permissão para acessar essa aba.');
+        return;
+    }
+
+    if (tabName === 'Finance' && !globalRoles.includes('Finance')) {
+        alert('Você não tem permissão para acessar essa aba.');
+        return;
+    }
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -12,4 +37,16 @@
     evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
+async function initTabs() {
+    try {
+        await getUserRolesTab();
+        console.log('Função getUserRolesTab foi chamada com sucesso.', globalRoles);
+
+        const fakeEvent = { currentTarget: document.getElementById("My_expense") };
+        openTab(fakeEvent, 'My_expense');
+    } catch (error) {
+        console.error('Ocorreu um erro ao chamar getUserRolesTab:', error);
+    }
+}
+
+initTabs();
